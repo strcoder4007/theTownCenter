dcb3e98f6f89bbd70b93d75d9927be3f6518a191
Shubham Singh <mynameshubham@gmail.com>
2017-08-14 06:43:36 +0400

    added all the dependencies and fonts; good to go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+
+
+const appRoutes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'about', component: AboutComponent},
+    {path:'resume', component: ResumeComponent},
+    {path:'**', redirectTo: '', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ResumeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
