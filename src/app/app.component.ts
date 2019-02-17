794decc2bab48cbab408ff0fecce73335b7cf818
Shubham Singh <mynameshubham@gmail.com>
2019-02-17 04:56:37 +0400

    Header and intro done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private router: Router) {
+    this.router = router;
+  }
+
+  headerClick(header) {
+    this.router.navigateByUrl('#' + header);
+    //alert(header);
+  }
+
+  ngOnInit() {
+  }
 }
