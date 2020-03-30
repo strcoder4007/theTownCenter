5b3bcc5191512b992b97847cc28a9e17c411b429
Shubham Singh <mynameshubham@gmail.com>
2020-03-30 12:32:50 +0400

    [Fixes]: Saved work exp in json, added doctorplan, CSS fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import { Http } from '@angular/http';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    workExp = [];
+    months = ["January","February","March","April","May","June","July", "August","September","October","November","December"];
     myProjects = [];
     myProjects1 = [];
     myProjects2 = [];
@@ -17,6 +19,39 @@ export class HomeComponent implements OnInit {
     buttonText = 'Load More';
 
     constructor(public http: Http) { }
+
+    get duration() {
+        if (this.workExp && this.workExp.length) {
+            let durations = [];
+            this.workExp.forEach(exp => {
+                const start = exp.duration.split(' - ')[0];
+                const startMonth = this.months.indexOf(start.split(' ')[0]);
+                const startYear = parseInt(start.split(' ')[1]);
+
+                let end = exp.duration.split(' - ')[1];
+                if (end === 'present')
+                    end = this.months[new Date().getMonth()] + ' ' + new Date().getFullYear();
+                const endMonth = this.months.indexOf(end.split(' ')[0]);
+                const endYear = parseInt(end.split(' ')[1]);
+
+                let totalMonths = 0;
+                let duration = '';
+
+                if (startYear === endYear) totalMonths = endMonth - startMonth;
+                else totalMonths = (12 - startMonth + endMonth) + ((endYear - startYear - 1)*12)
+                ++totalMonths;
+
+                if (totalMonths >= 12) {
+                    duration = (Math.floor(totalMonths/12) === 1) ? Math.floor(totalMonths/12) + ' year' : Math.floor(totalMonths/12) + ' years';
+                    if (totalMonths%12 !== 0)
+                        duration += ' ' + ((totalMonths%12 === 1) ? totalMonths%12 + ' month' : totalMonths%12 + ' months');
+                } else duration = (totalMonths === 1) ? totalMonths + ' month' : totalMonths + ' months';
+                durations.push(duration);
+            });
+            return durations;
+        }
+      return  ['', '', '', ''];
+    }
 
     loadMore() {
         if (this.buttonText == 'Show Less') {
@@ -33,9 +68,11 @@ export class HomeComponent implements OnInit {
     }
 
     processJson() {
-        this.getposts().subscribe((projects) => {
-            this.myProjects = projects;
-            // console.log(this.myProjects);
+        this.getposts().subscribe((allWork) => {
+            allWork.forEach(exp => {
+                if (exp.hasOwnProperty('company')) this.workExp.push(exp);
+                else this.myProjects.push(exp);
+            });
             for (let i  = 0; i < this.myProjects.length; i++) {
                 for (let j = 0; j < this.myProjects[i].tech.length - 1; j++) {
                     this.myProjects[i].tech[j] = this.myProjects[i].tech[j] + ',';
